d5ddc674a241061271454a0325424a43dd738bcd
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2025-09-29 15:05:29 +0300

    add delete button to table

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -1,12 +1,18 @@
 import classes from "./Table.module.scss";
 
 export default function Table() {
+  const deleteHandler = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
   return (
     <>
       {localStorage.getItem("name")}
       {localStorage.getItem("email")}
       {localStorage.getItem("age")}
       {localStorage.getItem("gender")}
+
+      <button onClick={deleteHandler}>Delete</button>
     </>
   );
 }
