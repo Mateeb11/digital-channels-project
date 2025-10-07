6fdb212cbc59ee96f35c2994051808aacbf85c17
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2025-10-07 08:26:10 +0300

    changed nav style to bs

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -5,27 +5,35 @@ import { Link, useLocation } from "react-router-dom";
 export default function Navigation() {
   const location = useLocation();
   return (
-    <nav>
-      <ul className={classes.nav}>
-        <li
-          className={`${
-            location.pathname !== "/digital-channels-project/table"
-              ? classes.active
-              : ""
-          }`}
-        >
-          <Link to={"/digital-channels-project/form"}>Form</Link>
-        </li>
-        <li
-          className={`${
-            location.pathname === "/digital-channels-project/table"
-              ? classes.active
-              : ""
-          }`}
-        >
-          <Link to={"/digital-channels-project/table"}>Table</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className={`card text-center`}>
+      <nav className={`card-header`}>
+        <ul className={`nav nav-tabs card-header-tabs`}>
+          <li className={`nav-item`}>
+            <Link
+              className={`nav-link ${
+                location.pathname !== "/digital-channels-project/table"
+                  ? "active"
+                  : ""
+              }`}
+              to={"/digital-channels-project/form"}
+            >
+              Form
+            </Link>
+          </li>
+          <li className={`nav-item`}>
+            <Link
+              className={`nav-link ${
+                location.pathname === "/digital-channels-project/table"
+                  ? "active"
+                  : ""
+              }`}
+              to={"/digital-channels-project/table"}
+            >
+              Table
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
